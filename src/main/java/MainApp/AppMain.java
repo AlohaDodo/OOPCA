a76f339c665912a4d686a40be34b967b75f40dbf
grimmy962 <145358834+grimmy962@users.noreply.github.com>
2025-02-27 22:44:26 +0000

    added basic menu system in AppMain
    - added first 6 choices for user to choose from based on the 6 features in the 1st milestone

--- src/main/java/MainApp/AppMain.java
+++ src/main/java/MainApp/AppMain.java
@@ -1,10 +1,32 @@
 package MainApp;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class AppMain {
+   //private MySqlAanimaDao animalDAO = new MySQLExpenseDao();
+    private Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String hello = "Hello world";
-        System.out.println(hello);
+        new AppMain().run();
     }
+
+    public void run() {
+        while(true){
+            System.out.println("Welcome to our Animal Shelter");
+            System.out.println("Menu:");
+            System.out.println("1) Display all animals");
+            System.out.println("2) Find animal");
+            System.out.println("3) Delete animal");
+            System.out.println("4) Add animal");
+            System.out.println("5) Update animal's info");
+            System.out.println("6) Get list of animals based by a Filter");
+            System.out.println("\n Enter your input: ");
+
+            int input = keyboard.nextInt();
+            keyboard.nextLine();
+           
+
+    }
+}
 }
