145bfa7888271acfd975fbcb382e38378cd626e8
SFuhad <166405754+SFuhad@users.noreply.github.com>
2025-03-07 20:49:07 +0000

    MySqlDao.java
    MySqlDao with the connector finished.

--- src/main/java/DAOs/MySqlDao.java
+++ src/main/java/DAOs/MySqlDao.java
@@ -1,5 +1,20 @@
 package DAOs;
 
-public class MySqlDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySqlDao {
+    private String url = "jdbc:mysql://localhost:3306/user_database";
+    private String username = "root";
+    private String password = "";
+
+    Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
 }
